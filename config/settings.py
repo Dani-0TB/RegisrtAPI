1bb39370cf361573eda72039700f0316b644c385
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2023-12-12 09:03:41 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -122,9 +122,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8100",
-    "https://regirst-api.onrender.com:10000"
+    "https://regirst-api.onrender.com:10000",
 ]
 
 REST_FRAMEWORK = {
